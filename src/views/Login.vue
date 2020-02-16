afceae75e49bea0b0912fcc70cc34066db4e38f0
sck <owecnv@qq.com>
2020-02-16 23:23:37 +0800

    dev

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="login">
+<el-button type="info" plain size="mini">dg</el-button>
+</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+#login {
+    
+}
+</style>
